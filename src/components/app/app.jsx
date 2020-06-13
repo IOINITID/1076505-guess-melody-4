b1dec9fd9fa6d0d041d6d92123e0c0b530062ccf
IOINITID <50173108+IOINITID@users.noreply.github.com>
2020-06-13 12:33:47 +0300

    Added library enzyme and covered components with e2e tests

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
+const welcomeButtonHandler = () => {};
+
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount}/>
+    <WelcomeScreen errorsCount={errorsCount} onWelcomeButtonClick={welcomeButtonHandler}/>
   );
 };
 
 App.propTypes = {
-  errorsCount: PropTypes.number.isRequired
+  errorsCount: PropTypes.number.isRequired,
 };
 
 export default App;
--- src/components/welcome-screen/welcome-screen.test.js
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -3,7 +3,7 @@ import rerender from 'react-test-renderer';
 import WelcomeScreen from './welcome-screen.jsx';
 
 it(`WelcomeScreen should render correctly`, () => {
-  const tree = rerender.create(<WelcomeScreen errorsCount={3}/>).toJSON();
+  const tree = rerender.create(<WelcomeScreen errorsCount={3} onWelcomeButtonClick={() => {}}/>).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
